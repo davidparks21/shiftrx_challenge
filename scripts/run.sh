3f32d5a1b86e7efba183ff062072c2af5d0f2263
davidparks21 <davidparks21@gmail.com>
2025-11-06 10:36:20 -0800

    small updates

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,8 +3,9 @@
 # Script Name   : run.sh
 # Description   : Starts the web service inside its docker container at
 #                 davidparks21/shiftrx_challenge:latest, runs web services
-#                 on port 5000.
+#                 on port 5000. Intended for local dev use only, the local
+#                 app folder is mounted over the docker container files.
 # Usage         : scripts/run.sh
 #===============================================================================
 
-docker run --rm -it --network host --publish 5000:5000 davidparks21/shiftrx_challenge:latest
+docker run --rm -it --network host -v "$(pwd)":/shiftrx_challenge davidparks21/shiftrx_challenge:latest
